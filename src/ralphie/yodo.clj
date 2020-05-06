2ba4a4d74b9ce78368bee72c3184d5ac54d7f2d6
Russell Matney <russell.matney@gmail.com>
2020-05-05 20:39:32 -0400

    feat: add support for restarting both

--- src/ralphie/yodo.clj
+++ src/ralphie/yodo.clj
@@ -4,12 +4,15 @@
    [ralphie.tmux :as tmux]))
 
 (defn restart-handler [_config _parsed]
+  ;; TODO send to yodo workspace
   (rofi/rofi {:msg "Restart what?"}
              [{:label     "Restart yodo server"
                ;; missed arity here does not get applied well
                :on-select #(do % (tmux/fire "sc --user restart yodo"))}
               {:label     "Restart yodo frontend"
-               :on-select #(do % (tmux/fire "sc --user restart yodo-fe"))}]))
+               :on-select #(do % (tmux/fire "sc --user restart yodo-fe"))}
+              {:label     "Why not both?"
+               :on-select #(do % (tmux/fire "sc --user restart yodo yodo-fe"))}]))
 
 (comment
   (restart-handler nil nil))
