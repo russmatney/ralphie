ae142ebd2a2903dd4950d0353cf327261e9a01ca
Russell Matney <russell.matney@gmail.com>
2020-05-09 19:03:49 -0400

    feat: add org parser

--- src/ralphie/config.clj
+++ src/ralphie/config.clj
@@ -1,13 +1,16 @@
 (ns ralphie.config
   (:require [ralphie.sh :refer [expand]]))
 
+;; TODO support configuration
+
 (defn home-dir [] (expand "~"))
 
 (defn project-dir [] (expand "~/russmatney/ralphie"))
 
-;; TODO determine this at runtime
 (defn github-username [] "russmatney")
 
 ;; TODO handle multiple monitors
 (defn monitor [] "eDP-1")
 ;; (defn monitor [] "HDMI-0")
+
+(defn org-dir [] (expand "~/todo"))
