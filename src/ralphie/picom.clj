5c404dfde7e56c0a371bf43fd0661d42c8782ed8
Russell Matney <russell.matney@gmail.com>
2020-07-24 12:24:00 -0400

    feat: add picom command

--- src/ralphie/picom.clj
+++ src/ralphie/picom.clj
@@ -1,16 +1,32 @@
 (ns ralphie.picom
   (:require
    [ralphie.command :refer [defcom]]
+   [ralphie.rofi :as rofi]
    [clojure.java.shell :as sh]))
 
 (defcom inc-opacity-command
   {:name          "opacity-increase"
    :one-line-desc "Increases the opacity of the next app clicked"
    :handler
-   (fn [_ _] (sh/sh "picom-trans" "-s" "+5"))})
+   (fn [_ _]
+     (sh/sh "picom-trans" "-s" "+5"))})
 
 (defcom dec-opacity-command
   {:name          "opacity-decrease"
    :one-line-desc "Decreases the opacity of the next app clicked"
    :handler
    (fn [_ _] (sh/sh "picom-trans" "-s" "-5"))})
+
+(defcom picom
+  {:name "picom"
+   :handler
+   (fn [_ _]
+     (rofi/rofi
+       {:msg "select"}
+       [{:label     "Shout about it!"
+         :on-select (fn [_]
+                      (sh/sh "notify-send" "HOWDY WORLD!"))}
+        {:label     "increase opacity"
+         :on-select (fn [_] (sh/sh "picom-trans" "-s" "-5"))}
+        {:label     "decrease opacity"
+         :on-select (fn [_] (sh/sh "picom-trans" "-s" "+5"))}]))})
