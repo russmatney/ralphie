bcae0d3280afc42fd07bb21a31cf0119eff60d09
Russell Matney <russell.matney@gmail.com>
2021-03-21 14:13:23 -0400

    feat: notify using notify-send.py and --replaces-process
    Notify now supports {:replaces-process "blah"} for updating a previously
    sent notification.
    
    Replaces-process is blocking, so for a quick impl I just don't
    process/check it. Not perfect, as passing :replaces-process now won't
    receive error messages for malformed notifies.
    
    I'm also not sure when these 'blocking' notifies get dealt with. is this
    a memory leak? Do they just timeout after a few minutes?

--- src/ralphie/notify.clj
+++ src/ralphie/notify.clj
@@ -1,28 +1,42 @@
 (ns ralphie.notify
   (:require
-   [babashka.process :refer [$ check]]))
+   [babashka.process :as process :refer [$ check]]))
 
 (defn notify
   ([notice]
    (cond (string? notice) (notify notice nil)
 
          (map? notice)
-         (let [{:keys [subject body]} notice]
-           (notify subject body))
+         (let [{:keys [subject body] :as opts} notice]
+           (notify subject body opts))
 
          :else
          (notify "Malformed notify call" "Expected string or map.")
          ))
 
-  ([subject body]
-   (-> ($ notify-send ~subject ~body)
-       check
-       :out
-       slurp)))
+  ([subject body & args]
+   (let [{:keys [replaces-process]}
+         (some-> args first)
+         exec-strs (cond-> ["notify-send.py" subject]
+                     body (conj body)
+                     replaces-process
+                     (conj "--replaces-process" replaces-process))
+         proc      (process/process (conj exec-strs) {:out :string})]
+     (println exec-strs)
+     ;; we only check when --replaces-process is not passed
+     ;; ... skips error messages if bad data is passed
+     ;; ... also not sure when these get dealt with. is this a memory leak?
+     (when-not replaces-process
+       (-> proc check :out))
+     nil)))
 
 (comment
   (notify "subj" "body\nbody\nbody")
+  (notify {:subject "subj"})
+  (notify "subj" "body\nbodybodddd" {:replaces-process "blah"})
+
   (notify {:subject "subj" :body {:value "v" :label "laaaa"}})
   (notify {:subject "subj" :body "BODY"})
   (-> ($ notify-send subj body)
-      check))
+      check)
+  )
