c9c7224bb94dc084887a72fa911ab7fcb43f0c62
Russell Matney <russell.matney@gmail.com>
2021-04-25 16:00:12 -0400

    feat: pulseaudio namespace, default sink vol fn
    Refactors the spotify ns, moving the pulseaudio funcs into their own.
    Adds a quick helper for exposing the 'last' sink's volume line. Def
    could get more feature-full here.

---
+++ src/ralphie/pulseaudio.clj
@@ -0,0 +1,136 @@
+(ns ralphie.pulseaudio
+  (:require
+   [babashka.process :as process]
+   [clojure.string :as string]
+   [clojure.edn :as edn]))
+
+
+;; https://askubuntu.com/questions/878853/change-spotify-volume-with-dbus
+;; #!/bin/bash
+;; spotify=$(pacmd list-sink-inputs | tr '\n' '\r' | perl -pe 's/ *index: ([0-9]+).+?application\.process\.binary = "([^\r]+)"\r.+?(?=index:|$)/\2 : \1\r/g' | tr '\r' '\n' | awk '/spotify/{print $3}')
+;; step=2
+;; pactl set-sink-input-volume $spotify +${step}%
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn ->sink-input-props
+  "Parses the properties: section of `pacmd list-sink-inputs`.
+  Assumes it is passed a list of trimmed strs,
+  with the properties: section as the tail."
+  [strs]
+  (->>
+    strs
+    (partition-by #(re-seq #"properties:" %))
+    last
+    (map (fn [s]
+           (-> s
+               (string/replace " =" "")
+               (#(str ":" %)))))
+    seq
+    (string/join " ")
+    (#(str "{" % "}"))
+    edn/read-string))
+
+(defn ->sink-data
+  "Parses the non-properties section of `pacmd list-sink-inputs`.
+  "
+  [strs]
+  (->>
+    strs
+    (partition-by #(re-seq #"properties:" %))
+    first
+    ;; here we drop lines without colons,
+    ;; then parse into key/values using the first colon found
+    (keep #(when (re-seq #":" %)
+             (string/split % #": " 2)))
+    (map (fn [[k v]]
+           [(-> k
+                (string/replace " " "-")
+                keyword)
+            v]))
+    (into {})))
+
+(defn ->sink-input [strs]
+  (merge
+    (->sink-data strs)
+    {:properties (->sink-input-props strs)}))
+
+(defn pacmd-sink-inputs []
+  (->
+    ^{:out :string}
+    (process/$ pacmd list-sink-inputs)
+    (process/check)
+    :out
+    (string/split #"\n")
+    (->>
+      (drop 1) ;; drop summary line
+      (map string/trim)
+      (partition-by #(re-seq #"^index:" %))
+      (partition 2 2)
+      (map (comp ->sink-input flatten)))))
+
+(comment
+  (pacmd-sink-inputs))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Pactl sinks
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn parse-sink
+  "Very basic parse of a few lines of the 'pactl list sinks' output"
+  [lines]
+  (->> lines
+       (map string/trim)
+       (keep #(when (re-seq #":" %)
+                (string/split % #": " 2)))
+       (map (fn [[k v]]
+              [(-> k
+                   (string/replace " " "-")
+                   keyword)
+               v]))
+       (into {})))
+
+(defn pactl-list-sinks
+  []
+  (->
+    ^{:out :string}
+    (process/$ pactl list sinks)
+    (process/check)
+    :out
+    (string/split #"\n")
+    (->>
+      (partition-by #{""})
+      (remove (comp #{""} first))
+      (map parse-sink))))
+
+(defn default-sink-volume
+  "Returns the volume of the _last_ sink in `pactl list sinks`.
+  TODO pick a sink by card/something other than order.
+  "
+  []
+  (some->> (pactl-list-sinks)
+           last
+           :Volume))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; pactl set-sink-input-volume $spotify +${step}%
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn pactl-set-sink-input-volume
+  "Updates a passed sinks volume with the indicated value.
+  Expects to be handed a sink parsed by `pacmd-sink-inputs`.
+
+  Volume-str should be something like +5, -10, where the int corresponds to a
+  percentage increase or decrease."
+  [{:keys [index]} volume-str]
+  (->
+    ^{:out :string}
+    (process/$ pactl set-sink-input-volume ~index ~(str volume-str "%"))
+    (process/check)
+    :out))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- src/ralphie/spotify.clj
+++ src/ralphie/spotify.clj
@@ -2,74 +2,8 @@
   (:require
    [ralph.defcom :refer [defcom]]
    [ralphie.notify :as notify]
-   [babashka.process :as process]
-   [clojure.string :as string]
-   [clojure.edn :as edn]))
+   [ralphie.pulseaudio :as pulseaudio]))
 
-;; https://askubuntu.com/questions/878853/change-spotify-volume-with-dbus
-;; #!/bin/bash
-;; spotify=$(pacmd list-sink-inputs | tr '\n' '\r' | perl -pe 's/ *index: ([0-9]+).+?application\.process\.binary = "([^\r]+)"\r.+?(?=index:|$)/\2 : \1\r/g' | tr '\r' '\n' | awk '/spotify/{print $3}')
-;; step=2
-;; pactl set-sink-input-volume $spotify +${step}%
-
-(defn ->sink-input-props
-  "Parses the properties: section of `pacmd list-sink-inputs`.
-  Assumes it is passed a list of trimmed strs,
-  with the properties: section as the tail."
-  [strs]
-  (->>
-    strs
-    (partition-by #(re-seq #"properties:" %))
-    last
-    (map (fn [s]
-           (-> s
-               (string/replace " =" "")
-               (#(str ":" %)))))
-    seq
-    (string/join " ")
-    (#(str "{" % "}"))
-    edn/read-string))
-
-(defn ->sink-data
-  "Parses the non-properties section of `pacmd list-sink-inputs`.
-  "
-  [strs]
-  (->>
-    strs
-    (partition-by #(re-seq #"properties:" %))
-    first
-    ;; here we drop lines without colons,
-    ;; then parse into key/values using the first colon found
-    (keep #(when (re-seq #":" %)
-             (string/split % #": " 2)))
-    (map (fn [[k v]]
-           [(-> k
-                (string/replace " " "-")
-                keyword)
-            v]))
-    (into {})))
-
-(defn ->sink-input [strs]
-  (merge
-    (->sink-data strs)
-    {:properties (->sink-input-props strs)}))
-
-(defn pacmd-sink-inputs []
-  (->
-    ^{:out :string}
-    (process/$ pacmd list-sink-inputs)
-    (process/check)
-    :out
-    (string/split #"\n")
-    (->>
-      (drop 1) ;; drop summary line
-      (map string/trim)
-      (partition-by #(re-seq #"^index:" %))
-      (partition 2 2)
-      (map (comp ->sink-input flatten)))))
-
-(comment
-  (pacmd-sink-inputs))
 
 (defn spotify-sync-input
   "Sources and parses `pacmd list-sink-inputs`.
@@ -77,25 +11,10 @@
   "
   []
   (some->>
-    (pacmd-sink-inputs)
+    (pulseaudio/pacmd-sink-inputs)
     (filter (comp #{"RUNNING"} :state))
     (filter (comp #{"Spotify" "spotify"} :application.name :properties))
     first))
-
-;; pactl set-sink-input-volume $spotify +${step}%
-
-(defn pactl-set-sink-input-volume
-  "Updates a passed sinks volume with the indicated value.
-  Expects to be handed a sink parsed by `pacmd-sink-inputs`.
-
-  Volume-str should be something like +5, -10, where the int corresponds to a
-  percentage increase or decrease."
-  [{:keys [index]} volume-str]
-  (->
-    ^{:out :string}
-    (process/$ pactl set-sink-input-volume ~index ~(str volume-str "%"))
-    (process/check)
-    :out))
 
 (defcom spotify-volume
   {:defcom/name "spotify-volume"
@@ -117,7 +36,7 @@
                            :spotify-input spotify-input}))
          (do
            (println "adjusting spotify-volume" {:arg arg :volume volume-str})
-           (pactl-set-sink-input-volume spotify-input volume-str)
+           (pulseaudio/pactl-set-sink-input-volume spotify-input volume-str)
            (notify/notify
              {:notify/subject "adjusted spotify-volume"
               :notify/body (str "new volume: " (:volume spotify-input))
