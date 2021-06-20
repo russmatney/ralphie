04b0cd25e2548c18679fa0da8160db310a1bf83f
Russell Matney <russell.matney@gmail.com>
2021-06-20 17:41:02 -0400

    feat: genericize install-zsh-completion for any defcom

--- src/ralphie/install.clj
+++ src/ralphie/install.clj
@@ -16,21 +16,26 @@
 ;; Install zsh completion
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn zsh-completion-path []
-  (str (config/home-dir) "/.zsh/completion/_ralphie"))
+(defn zsh-completion-path [bin-name]
+  (str (config/home-dir) "/.zsh/completion/_" bin-name))
 
-(defcom install-zsh-completion
+(defn install-zsh-completion
+  "Creates and installas a zsh-completion file for all required `defcom`s,
+  at the supplied bin-name. (ex. `ralphie` or `clawe`)."
+  [bin-name]
   (let [cmds-string
         (->> (defcom/list-commands)
              (map :name)
              (string/join " \\
 "))
-        completion-file (str "#compdef _ralphie ralphie
-
+        completion-file (str "#compdef _" bin-name " " bin-name "
 _arguments -C \\
   \"1: :(
 " cmds-string ")\"")]
-    (spit (zsh-completion-path) completion-file)))
+    (spit (zsh-completion-path bin-name) completion-file)))
+
+(defcom install-ralphie-zsh-completion
+  (install-zsh-completion "ralphie"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Build uberscript
